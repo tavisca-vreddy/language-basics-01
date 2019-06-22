5990714fdd817ca5d8a2183194e941833738a1de
vijay <vreddy@tavisca.in>
2019-06-22 11:59:44 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,7 +23,74 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            string result = "0", k;
+            int l1, l2, m;
+            string[] a = equation.Split(new Char[] { '*', '=' });
+            if (a[2].Contains("?"))
+            {
+                l1 = Int32.Parse(a[0]);
+                l2 = Int32.Parse(a[1]);
+                m = l1 * l2;
+                k = m.ToString();
+                if (k.Length != a[2].Length)
+                    return -1;
+                else
+                {
+                    for (int x = 0; x < k.Length; x++)
+                        if (k[x] == a[2][x])
+                            continue;
+                        else if (a[2][x] == '?')
+                            result = k[x].ToString();
+                        else
+                            return -1;
+                }
+            }
+            else if (a[0].Contains("?"))
+            {
+                l2 = Int32.Parse(a[1]);
+                m = Int32.Parse(a[2]);
+                if (m % l2 != 0)
+                    return -1;
+                l1 = m / l2;
+                k = l1.ToString();
+                if (k.Length != a[0].Length)
+                    return -1;
+                else
+                {
+                    for (int x = 0; x < k.Length; x++)
+                        if (k[x] == a[0][x])
+                            continue;
+                        else if (a[0][x] == '?')
+                            result = k[x].ToString();
+                        else
+                            return -1;
+                }
+
+
+            }
+            else
+            {
+                l1 = Int32.Parse(a[0]);
+                m = Int32.Parse(a[2]);
+                if (m % l1 != 0)
+                    return -1;
+                l2 = m / l1;
+                k = l2.ToString();
+                if (k.Length != a[1].Length)
+                    return -1;
+                else
+                {
+                    for (int x = 0; x < k.Length; x++)
+                        if (k[x] == a[1][x])
+                            continue;
+                        else if (a[1][x] == '?')
+                            result = k[x].ToString();
+                        else
+                            return -1;
+                }
+            }
+            return Int32.Parse(result);
+            //throw new NotImplementedException();
         }
     }
 }
